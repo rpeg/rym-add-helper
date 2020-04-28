2b5dd1d02fa7838160e6333ad800c387bdc201cf
Sean Johnson <sean.johnson.ca@gmail.com>
2020-04-28 11:17:29 -0700

    add dist to repo

---
+++ dist/chrome/content.js
@@ -0,0 +1,79 @@
+/* eslint-disable strict */
+
+'use strict';
+
+const injectScript = (filename) => {
+  const script = document.createElement('script');
+  script.setAttribute('type', 'module');
+  script.setAttribute('src', chrome.extension.getURL(filename));
+  const head = document.head || document.getElementsByTagName('head')[0] || document.documentElement;
+  head.insertBefore(script, head.lastChild);
+};
+
+/**
+ * Won't work for every possible second-level domain, but should be good enough
+ * for the purpose of skipping subdomains with common SLDs.
+ *
+ * e.g. "band.bandcamp.com" -> "bandcamp"
+ */
+const getDomainName = (host) => {
+  const TLD_RE = /^(com|edu|gov|net|mil|org|nom|co|name|info|biz)$/i;
+
+  const parts = host.split('.').reverse();
+
+  if (parts.length >= 3) { // has subdomain
+    if (parts[0].match(TLD_RE)) {
+      return parts[1];
+    }
+
+    if (parts[1].match(TLD_RE)) {
+      return parts[0];
+    }
+  }
+
+  return parts[1];
+};
+
+// route messages between modules and background script
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+  switch (request.type) {
+    case 'toggle': {
+      const domain = getDomainName(window.location.host);
+
+      chrome.storage.sync.get([domain], (result) => {
+        result[domain] && console.info(`${domain} templated loaded`);
+
+        window.postMessage({
+          storedTemplate: result[domain],
+          ...request,
+        });
+      });
+
+      break;
+    }
+    default:
+      window.postMessage(request);
+  }
+});
+
+window.addEventListener('message', (message) => {
+  if (message.source !== window) return;
+
+  switch (message.data.type) {
+    case 'setStorage': {
+      chrome.storage.sync.set({ [message.data.key]: message.data.value }, () => {
+        console.info(`${message.data.key} template saved`);
+      });
+      break;
+    }
+    case 'rym_submit':
+      chrome.runtime.sendMessage(message.data);
+      break;
+    default:
+      chrome.runtime.sendMessage(message.data);
+      break;
+  }
+}, false);
+
+injectScript('main.js');
+injectScript('fill.js');
